1a306759baddd98a86d724d316c4b0be6d0dcf5b
Prakash7920 <prakashji792000@gmail.com>
2026-07-14 20:41:32 +0530

    Implement dynamic navbar styling on scroll
    Add scroll event listener to change navbar style

--- js/common.js
+++ js/common.js
@@ -1,1 +1,17 @@
+window.addEventListener("scroll", function () {
 
+    const nav = document.querySelector(".navbar");
+
+    if (window.scrollY > 80) {
+
+        nav.style.background = "#0B7A3D";
+        nav.style.boxShadow = "0 5px 20px rgba(0,0,0,.25)";
+
+    } else {
+
+        nav.style.background = "rgba(11,122,61,.95)";
+        nav.style.boxShadow = "none";
+
+    }
+
+});
