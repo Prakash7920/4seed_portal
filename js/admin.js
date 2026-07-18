f82341ce82a9beb6fd271a2f392362e92b9f9506
Prakash7920 <prakashji792000@gmail.com>
2026-07-18 22:23:36 +0530

    Implement fetch and display for partners
    Fetch partners data from the web app and display in table.

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,47 @@
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbw9P5iUDKYl3nXAaFfTdEO_rf7PfHSiLkwTjXq7HIpic7tOdg85aqIIeexbF63qrzIU/exec";
 
+fetch("https://script.google.com/macros/s/AKfycbw9P5iUDKYl3nXAaFfTdEO_rf7PfHSiLkwTjXq7HIpic7tOdg85aqIIeexbF63qrzIU/exec",{
+    method:"POST",
+    body:JSON.stringify({
+        action:"getAllPartners"
+    })
+})
+.then(res=>res.json())
+.then(data=>{
+
+    if(data.success){
+
+        loadPartners(data.partners);
+
+    }
+
+});
+function loadPartners(partners){
+
+    const tbody = document.querySelector("#partnerTable tbody");
+
+    tbody.innerHTML = "";
+
+    partners.forEach(partner=>{
+
+        tbody.innerHTML += `
+        <tr>
+
+            <td>${partner.partnerId}</td>
+
+            <td>${partner.name}</td>
+
+            <td>₹${partner.wallet}</td>
+
+            <td>${partner.status}</td>
+
+            <td>
+                <button>Edit</button>
+            </td>
+
+        </tr>
+        `;
+
+    });
+
+}
