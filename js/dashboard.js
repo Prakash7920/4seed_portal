b7f19de3cf20966fcfd8434a8efe5b5ec1549b95
Prakash7920 <prakashji792000@gmail.com>
2026-07-18 23:52:56 +0530

    Update fetch URL for withdrawal request

--- js/dashboard.js
+++ js/dashboard.js
@@ -54,7 +54,7 @@ function sendWithdrawRequest(){
         return;
     }
 
-    fetch(WEB_APP_URL,{
+    fetch("https://script.google.com/macros/s/AKfycbw9P5iUDKYl3nXAaFfTdEO_rf7PfHSiLkwTjXq7HIpic7tOdg85aqIIeexbF63qrzIU/exec',{
         method:"POST",
         body:JSON.stringify({
             action:"withdrawRequest",
