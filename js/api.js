727d57140ef87a0295ed60bb2f3f703d1713a48a
Prakash7920 <prakashji792000@gmail.com>
2026-07-27 22:11:43 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -4,7 +4,7 @@
 // ================================
 
 const WEB_APP_URL =
-"https://script.google.com/macros/s/AKfycbxtg8NM_aQxAxw5IjsP5oeKsDe3qQbVpGLL2qk70HvPAvWF-5fKvhNYUox6Egsa29VJ/exec";
+"https://script.google.com/macros/s/AKfycbzo36Z1_t1_dzpEPd4tNWv7vZjRv9zcGOLvNf5m048Yy1CY_A29JtZqZDQhx5lYqNbV/exec";
 
 // Verify Sponsor
 async function verifySponsor(sponsorId){
