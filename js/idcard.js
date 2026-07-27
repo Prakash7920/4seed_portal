213db2bcb3eadee152e8f787aa8c8466dfdea762
Prakash7920 <prakashji792000@gmail.com>
2026-07-27 22:20:09 +0530

    Update idcard.js

--- js/idcard.js
+++ js/idcard.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwvrf98i4rL0atUtIRF1J6dtbS44EcQGnNehURdKYPmzltCYp9nvZc6dNjh6F0lGorI/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzo36Z1_t1_dzpEPd4tNWv7vZjRv9zcGOLvNf5m048Yy1CY_A29JtZqZDQhx5lYqNbV/exec";
 
 document.addEventListener("DOMContentLoaded", loadIdCard);
 
