474012946e4402e9094f94146621d2e4845aa0d4
Prakash7920 <prakashji792000@gmail.com>
2026-07-29 23:42:20 +0530

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzo36Z1_t1_dzpEPd4tNWv7vZjRv9zcGOLvNf5m048Yy1CY_A29JtZqZDQhx5lYqNbV/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzwgy9llpVtRIX2KCiFEdmLp9yDqQQCaK4qqzeFmjN351tlu_BDP425Ry0Yfgg3BCcq/exec";
 
 function adminLogin() {
     const username = document.getElementById("username").value.trim();
