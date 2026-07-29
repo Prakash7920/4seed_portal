98fb96fc67c92a4aa6c2c6e424d8332ea17ed36b
Prakash7920 <prakashji792000@gmail.com>
2026-07-29 22:00:37 +0530

    Delete js/auth.js

--- js/auth.js
+++
@@ -1,9 +0,0 @@
-// ===============================
-// AUTH GUARD
-// ===============================
-
-const partner = JSON.parse(localStorage.getItem("partner"));
-
-if (!partner) {
-    window.location.href = "login.html";
-}
