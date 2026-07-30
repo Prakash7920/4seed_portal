f0bab33a4ce38dd0ad60f0874303249ff52c85d3
Prakash7920 <prakashji792000@gmail.com>
2026-07-30 19:34:10 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzwgy9llpVtRIX2KCiFEdmLp9yDqQQCaK4qqzeFmjN351tlu_BDP425Ry0Yfgg3BCcq/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycby0gIWVUif3O6Vl1x4ymG8oJMXSTuIHU42pKQDibdXukTfU5uZO5TzYjOIvSX1_kk9X/exec";
 
 document
 .getElementById("loginForm")
@@ -17,7 +17,7 @@ async function login(e){
 
     try{
 
-        const response = await fetch("https://script.google.com/macros/s/AKfycbzwgy9llpVtRIX2KCiFEdmLp9yDqQQCaK4qqzeFmjN351tlu_BDP425Ry0Yfgg3BCcq/exec",{
+        const response = await fetch(WEB_APP_URL,{
             method:"POST",
             body:JSON.stringify({
                 action:"login",
